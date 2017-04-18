ab83f6fe39433871c18cd7dc2d52798e4ecf589f
Jeff Pritchard <Jpritchard0085@conestogac.on.ca>
2017-04-18 12:30:38 -0400

    Jeff's base files

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@
 
 		$connectionString = 'odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\\xampp\\htdocs\\Assignment5\\Data\\assignment5.mdb';
 
-		$connection = new PDO($connectionString);
+		$connection = new PDO($connectionString) or die;
 		$connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 		return $connection;
--- vendorTableData.php
+++ vendorTableData.php
@@ -9,19 +9,36 @@
 
 		$connection = ConnectToDatabase();
 
-		$querySelect = "SELECT VendorName FROM Vendors";
+		$querySelect = "SELECT * FROM Vendors";
 		$preparedQuerySelect = $connection -> prepare($querySelect);
 		$preparedQuerySelect -> execute();
 
 		while ($row = $preparedQuerySelect -> fetch())
 		{
 
-			$vendorNo = "";
+			$vendorNo = $row['VendorNo'];
+			$vendorNo = round($vendorNo);
 			$vendorName = $row['VendorName'];
+			$address1 = $row['Address1'];
+			$address2 = $row['Address2'];
+			$city = $row['City'];
+			$province = $row['Prov'];
+			$postalCode = $row['PostCode'];
+			$country = $row['Country'];
+			$phone = $row['Phone'];
+			$fax = $row['Fax'];
 
 			$tableBodyText .= "<tr>";
 			$tableBodyText .= "<td>$vendorNo</td>";
 			$tableBodyText .= "<td class='text'>$vendorName</td>";
+			$tableBodyText .= "<td class='text'>$address1</td>";
+			$tableBodyText .= "<td class='text'>$address2</td>";
+			$tableBodyText .= "<td class='text'>$city</td>";
+			$tableBodyText .= "<td class='text'>$province</td>";
+			$tableBodyText .= "<td class='text'>$postalCode</td>";
+			$tableBodyText .= "<td class='text'>$country</td>";
+			$tableBodyText .= "<td class='text'>$phone</td>";
+			$tableBodyText .= "<td class='text'>$fax</td>";
 			$tableBodyText .= "</tr>";
 
 		}
@@ -37,7 +54,6 @@
 		$text = "<tr id='tableHeader'>";
 		$text .= "<th>VendorNo</th>";
 		$text .= "<th>VendorName</th>";
-/*
 		$text .= "<th>Address1</th>";
 		$text .= "<th>Address2</th>";
 		$text .= "<th>City</th>";
@@ -46,7 +62,7 @@
 		$text .= "<th>Count</th>";
 		$text .= "<th>Phone</th>";
 		$text .= "<th>Fax</th>";
-*/
+
 		$text .= "</tr>";
 
 		echo $text;
