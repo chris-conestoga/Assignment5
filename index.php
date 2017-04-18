1a2c9ab32b92da34b82d0a97ed14f2c4abff6a22
chris-conestoga <chris.conetoga@gmail.com>
2017-04-18 19:55:22 -0400

    vendor table input fields

--- index.php
+++ index.php
@@ -15,16 +15,22 @@
 	<body>
 
 		<table>
-
 			<?php
-
 				CreateVendorTableHeader();
-				FillVendorTable();
-
+				/*FillVendorTable();*/
 			?>
-
+			<td class="input">[New]</td>
+			<td class="input"><input type="text" name="VendorName"></td>
+			<td class="input"><input type="text" name="Address1"></td>
+			<td class="input">eh</td>
+			<td class="input"><input type="text" name="City"></td>
+			<td class="input"><input type="text" name="Prov"></td>
+			<td class="input"><input type="text" name="PostCode"></td>
+			<td class="input"><input type="text" name="Count"></td>
+			<td class="input"><input type="text" name="Phone"></td>
+			<td class="input"><input type="text" name="Fax"></td>
 		</table>
-
+		<button onclick="">Add New Part</button>
 	</body>
 
 </html>
