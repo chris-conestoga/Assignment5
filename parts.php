a72ca22ca79bd562557566b146992daa9b14b019
JMPritchard <jeffpritchard7@gmail.com>
2017-04-19 18:28:59 -0400

    line 30 needs correct syntax

--- parts.php
+++ parts.php
@@ -1,28 +1,38 @@
 <?php
 	include("connection.php");
-	$valid = false;
 
-	print "<h3>Checking Data...</h3>";
+	print "<h3>Establishing Database connection...</h3>";
 
-	if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
-	{
-		$vendorNo = $_POST['VendorNo'];
-		$description = $_POST['Description'];
-		$onHand = $_POST['OnHand'];
-		$onOrder = $_POST['OnOrder'];
-		$cost = $_POST['Cost'];
-		$listPrice = $_POST['ListPrice'];
-		//validate data
-		//summarize data and set value to true or give error
+	PartVerify();
 
-		//$valid = true;
+	function PartVerify(){
+
+		print "<br/><br/><h3>Checking Data...</h3>";
+
+		if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
+		{
+			$vendorNo = $_POST['VendorNo'];
+			$description = $_POST['Description'];
+			$onHand = $_POST['OnHand'];
+			$onOrder = $_POST['OnOrder'];
+			$cost = $_POST['Cost'];
+			$listPrice = $_POST['ListPrice'];
+			//validate data
+			print "<h4>New row data: $vendorNo, $description, $onHand, $onOrder, $cost, $listPrice</h4>";
+
+			//if successful:
+			print "<br/><h3>New row being added to the Parts table...</h3>";
+			//connect to database
+			$connection = ConnectToDatabase();
+			//insert new data
+			$sql = "INSERT INTO Parts VALUES ('$vendorNo', '$description', '$onHand', '$onOrder', '$cost', '$listPrice')";
+			
+			$connection -> exec($sql);
+			//^^^this is the line that is bugged.
+
+			//else provide error:
+
+		}
 	}
-	if ($valid){
-		print "New Data being added to the Parts table...";
-		//connect to database
 
-		$connection = ConnectToDatabase();
-		//insert new data
-		INSERT INTO Parts VALUES ($vendorNo, $description, $onHand, $onOrder, $cost, $listPrice);
-	}
 ?>
