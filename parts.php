267de633b28c422e2e15cdb91a069a05eeaa876a
JMPritchard <jeffpritchard7@gmail.com>
2017-04-19 10:11:10 -0400

    Update parts.php

--- parts.php
+++ parts.php
@@ -1,11 +1,28 @@
 <?php
+	include("connection.php");
 	$valid = false;
+
+	print "<h3>Checking Data...</h3>";
+
 	if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
 	{
+		$vendorNo = $_POST['VendorNo'];
+		$description = $_POST['Description'];
+		$onHand = $_POST['OnHand'];
+		$onOrder = $_POST['OnOrder'];
+		$cost = $_POST['Cost'];
+		$listPrice = $_POST['ListPrice'];
 		//validate data
-		//if it validates, $valid will be set true
+		//summarize data and set value to true or give error
+
+		//$valid = true;
 	}
 	if ($valid){
-		//add the data to the database
+		print "New Data being added to the Parts table...";
+		//connect to database
+
+		$connection = ConnectToDatabase();
+		//insert new data
+		INSERT INTO Parts VALUES ($vendorNo, $description, $onHand, $onOrder, $cost, $listPrice);
 	}
 ?>
